38e94ca00c6f8ea82565c4457a4be340e60e8a10
Bryce Weber <bpweber98@gmail.com>
2019-10-05 01:20:17 -0700

    begin implementation of history feature

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include <sstream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <queue>
 #define MAX_LINE 80
 
 int main(){
     std::string args[MAX_LINE/2+1];     //used to store the args from a stringstream
     char *argc[MAX_LINE/2+1];           //used to call execvp()
+    std::queue<std::string> hist;
+    int num_cmds = 0;
     int should_run = 1;
     while(should_run){
         std::cout << "osh> ";
@@ -22,6 +25,35 @@ int main(){
         int nullterm = i;               //null term for the end of the command and args
         if(args[0] == "exit")           //just check if they want to exit now
             should_run = 0;
+        else if(args[0] == "history"){
+            std::queue<std::string> tmp(hist);
+            while(!tmp.empty()){
+                std::cout << " " << tmp.front() << std::endl;
+                tmp.pop();
+            }
+        }else if(args[0] == "!!"){
+            std::string t;
+            std::string last = hist.back();
+            std::stringstream lcs(last);
+            for(i = 0; i < MAX_LINE/2+1; i++)   //make sure to clear args[]
+                args[i] = "";
+            i = 0;
+            lcs >> t;
+            while(lcs >> args[i])            //parse user input into args[]
+                i++;
+            if(args[0] == "history"){
+                std::queue<std::string> tmp(hist);
+                while(!tmp.empty()){
+                    std::cout << " " << tmp.front() << std::endl;
+                    tmp.pop();
+                }
+            }
+            nullterm = i;
+            t = "";
+            for(i = 0; i < nullterm; i++)
+                t += args[i] + " ";
+            in = t;
+        }
         int to_wait = 1;                //should the parent wait for child to exit?
         for(int i = 0; i < MAX_LINE/2+1; i++)
             if(args[i] == "&")          //if there's an & then no!
@@ -34,6 +66,12 @@ int main(){
         else                            //parent
             if(to_wait)                 //didn't find an &, wait for child
                 wait(NULL);
+        num_cmds++;
+        std::ostringstream ncs;
+        ncs << num_cmds;
+        hist.push(ncs.str() + " " + in);
+        if(hist.size() > 10)
+            hist.pop();
     }
     return 0;
 }
